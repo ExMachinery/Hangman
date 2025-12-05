e570a055d1edd946c23eb697110db72ec017a1d3
ExMachinery <milkcarrotc@gmail.com>
2025-12-06 00:07:11 +0500

    Now for real (file where not saved)

--- lib/game.rb
+++ lib/game.rb
@@ -24,9 +24,52 @@ class Game
     
     put "All right, lets hang around. You need to guess THE WORD! Or I'll hang you."
     engine.start_round(player)
-    
 
-    
+    if engine.round.turns_left != 0
+      engine.round.turns_left -= 1
+      puts "You have #{engine.round.turns_left} turns left. Name a letter or type '1' to save and exit."
+      puts "=================="
+      puts "Word looks like this right now:"
+      engine.round.print_state
+      puts "=================="
+      puts "Letter you have used:"
+      engine.round.print_used
+      puts "=================="
+      
+      # Guess validation (It should be a method)
+      valid = false
+      result = nil
+      until valid
+        guess = gets.chomp
+        if guess == 1
+          valid = true
+          # Saving game code here. Saving condition.
+        elsif guess.match?(/^[a-zA-Z]$/)
+          guess = guess.downcase
+          if engine.round.used_letters.include?(guess)
+            puts "Letter used. Take one more shot."
+          else
+            valid = true
+            result = engine.process_turn(guess)
+          end
+        else
+          puts 'Come again?' 
+        end
+      end
+
+      # Result reaction
+      if result
+        puts "You're god damn right!"
+        if !engine.round.current_state.include?("_ ")
+          # Winning condition
+        end
+      else
+        puts "You shot. You miss."
+      end
+    else 
+      puts "You lost. You hanged. Not sorry."
+      # Loosing condition
+    end
   end
 
 end
